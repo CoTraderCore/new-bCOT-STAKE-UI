ecaaec4002bbc49c1a1889139ae3cfa83eb59eb7
github2k20 <priyansh.44rastogi@gmail.com>
2021-03-18 00:09:39 +0530

    Added basic form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from "react";
+import '../App.css'
 
 
-function App() {
-  return (
-    <div className="App">
-      Project Setup
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment >
+        <div
+          className="card col-lg-12 ml-auto mr-auto"
+          style={{ maxWidth: "300px"}}
+        >
+          <h1 className="cardheader text-center">Rover Capital</h1>
+          <form>
+            <div className="form-group mt-3">
+              <input
+                id="content"
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter Amount"
+                required
+              ></input>
+            </div>
+
+            <button className="btn btn-primary btn-block" type="submit">Deposit</button>
+          </form>
+        </div>
+      </React.Fragment >
+    );
+  }
 }
 
 export default App;
