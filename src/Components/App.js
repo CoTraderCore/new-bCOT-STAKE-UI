64abeb83e3a62b5295470b17ddb7509ca4f07ba6
github2k20 <priyansh.44rastogi@gmail.com>
2021-03-18 14:03:12 +0530

    Implemented Tab Switching

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,32 +1,17 @@
 import React, { Component } from "react";
-import '../App.css'
-
-
+import "../App.css";
+import ControlledTabs from './ControlledTabs'
 class App extends Component {
   render() {
     return (
-      <React.Fragment >
         <div
           className="card col-lg-12 ml-auto mr-auto"
-          style={{ maxWidth: "300px"}}
+          style={{ maxWidth: "320px" }}
         >
           <h1 className="cardheader text-center">Rover Capital</h1>
-          <form>
-            <div className="form-group mt-3">
-              <input
-                id="content"
-                type="text"
-                className="form-control"
-                id="name"
-                placeholder="Enter Amount"
-                required
-              ></input>
-            </div>
-
-            <button className="btn btn-primary btn-block" type="submit">Deposit</button>
-          </form>
+          <ControlledTabs/>
+          
         </div>
-      </React.Fragment >
     );
   }
 }
