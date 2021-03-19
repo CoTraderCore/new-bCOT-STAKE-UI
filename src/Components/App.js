e63c8a93614fe11f94dd9d3a30fbb2792b564efb
github2k20 <priyansh.44rastogi@gmail.com>
2021-03-19 20:54:00 +0530

    Added mobx state management

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import "../App.css";
 import ControlledTabs from "./ControlledTabs";
 import ConnectWallet from './ConnectWallets'
+import {walletStore} from "../models/wallet_model";
+
 class App extends Component {
   
   render() {
     
     return (
       <div className="connect text-center">
-        <ConnectWallet/>
+        <ConnectWallet store={walletStore}/>
         <div
           className="card col-lg-12 ml-auto mr-auto"
           style={{ maxWidth: "320px" }}
--- src/models/wallet_model.ts
+++ src/models/wallet_model.ts
@@ -3,73 +3,80 @@ import Web3Modal from "web3modal";
 import Authereum from "authereum";
 import Fortmatic from "fortmatic";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { useState } from "react";
+import { makeObservable, observable,autorun } from "mobx"
 
-export default function Wallet_Model() {
-  const [loading, setLoading] = useState(false);
+class WalletStore{
+  loading:boolean=false;
+  web3:unknown;
+
+  constructor() {
+    makeObservable(this, {
+      loading: observable,
+      web3: observable
+      });
+      autorun(async () => this.web3 = await this.getweb3());
+    }
+
   
-  return {
-    get web3Loading() {
-      return loading;
-    },
-    async getweb3() {
-      
-      setLoading(true);
-      let web3Modal;
-      let web3;
-      let providerOptions;
-      providerOptions = {
-        authereum: {
-          package: Authereum, // required
-        },
 
-        walletconnect: {
-          package: WalletConnectProvider, // required
-          options: {
-            infuraId: "https://mainnet.infura.io/v3/36b6921562ec407e826fb24562a5c9db" // required
-          }
-        },
+  async getweb3() {
+    this.loading=true;
+    let providerOptions;
+    let web3Modal;
+    let web3;
+    providerOptions = {
+      authereum: {
+        package: Authereum, // required
+      },
 
-        fortmatic: {
-          package: Fortmatic, // required
-          options: {
-            key: "pk_live_7E6A277E15DE415B" // required
-          }
+      walletconnect: {
+        package: WalletConnectProvider, // required
+        options: {
+          infuraId: "https://mainnet.infura.io/v3/36b6921562ec407e826fb24562a5c9db" // required
         }
-      };
+      },
 
-      
-      web3Modal = new Web3Modal({
-        network:
-          "mainnet",
-        cacheProvider: true,
-        providerOptions,
-      });
+      fortmatic: {
+        package: Fortmatic, // required
+        options: {
+          key: "pk_live_7E6A277E15DE415B" // required
+        }
+      }
+    };
 
-      const provider = await web3Modal.connect();
+    
+    web3Modal = new Web3Modal({
+      network:
+        "mainnet",
+      cacheProvider: true,
+      providerOptions,
+    });
 
-      provider.on("accountsChanged", (accounts: string[]) => {
-        console.log(accounts);
-      });
-      
-      // Subscribe to chainId change
-      provider.on("chainChanged", (chainId: number) => {
-        console.log(chainId);
-      });
-      
-      // Subscribe to provider connection
-      provider.on("connect", (info: { chainId: number }) => {
-        console.log(info);
-      });
-      
-      // Subscribe to provider disconnection
-      provider.on("disconnect", (error: { code: number; message: string }) => {
-        console.log(error);
-      });
-      
-      web3 = new Web3(provider);
-      setLoading(false);
-      return web3;
-    },
-  };
+    const provider = await web3Modal.connect();
+
+    provider.on("accountsChanged", (accounts: string[]) => {
+      console.log(accounts);
+    });
+    
+    // Subscribe to chainId change
+    provider.on("chainChanged", (chainId: number) => {
+      console.log(chainId);
+    });
+    
+    // Subscribe to provider connection
+    provider.on("connect", (info: { chainId: number }) => {
+      console.log(info);
+    });
+    
+    // Subscribe to provider disconnection
+    provider.on("disconnect", (error: { code: number; message: string }) => {
+      console.log(error);
+    });
+    
+    web3 = new Web3(provider);
+    this.loading=false;
+    return web3;
+  }
 }
+
+export const walletStore = new WalletStore();
