2c7c11880f0c3ab644f38a6cfcbb54768c9a1c69
github2k20 <priyansh.44rastogi@gmail.com>
2021-03-26 15:37:18 +0530

    Added UI

---
+++ src/components/CardNav/index.tsx
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { ButtonMenu, ButtonMenuItem } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
+
+const StyledNav = styled.div`
+  margin-bottom: 40px;
+`
+
+function Nav({ activeIndex=0 }: { activeIndex?: number }) {
+
+  const [index, setIndex] = useState(activeIndex);
+  const handleClick = (newIndex) => {
+    if(newIndex===4)
+    newIndex=3
+    setIndex(newIndex);console.log(newIndex)
+  };
+  const TranslateString = useI18n()
+  return (
+    <StyledNav>
+      <ButtonMenu activeIndex={index} onItemClick={(i)=>handleClick(i)} scale="sm" variant="subtle">
+      <ButtonMenuItem to="/deposit" as={Link}>
+          Deposit
+        </ButtonMenuItem>
+        <ButtonMenuItem to="/withdraw" as={Link}>
+       Withdraw
+        </ButtonMenuItem>
+        <ButtonMenuItem to="/swap" as={Link}>
+       Swap
+        </ButtonMenuItem>
+        <ButtonMenuItem to="/pool" as={Link}>
+        Liquidity
+        </ButtonMenuItem>
+        <ButtonMenuItem
+          id="pool-nav-link"
+          as="a"
+          href="https://www.binance.org/en/bridge?utm_source=PancakeSwap"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Bridge
+        </ButtonMenuItem>
+      </ButtonMenu>
+    </StyledNav>
+  )
+}
+
+export default Nav
