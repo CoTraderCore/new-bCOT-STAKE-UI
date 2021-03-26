479627b1bf8555faf871bd413fda0a3bfb9899d9
github2k20 <priyansh.44rastogi@gmail.com>
2021-03-26 14:02:48 +0530

    Final Commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import "./index.css";
 import App from "./Components/App";
 import {BrowserRouter} from 'react-router-dom'; 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Trial from './Components/Trial';
+
 
 ReactDOM.render( <BrowserRouter> <App /> </BrowserRouter> , document.getElementById("root"));
