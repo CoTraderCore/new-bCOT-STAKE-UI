2947b589732f0093a7558fe200b94a6137f28262
RuslanMirov <ruslan777mirov@gmail.com>
2021-05-03 09:24:23 +0300

    Remove social profile

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,7 +5,7 @@ import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
 import useGetPriceData from 'hooks/useGetPriceData'
-import useGetLocalProfile from 'hooks/useGetLocalProfile'
+// import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import useAuth from 'hooks/useAuth'
 import links from './config'
 
@@ -16,7 +16,7 @@ const Menu: React.FC = (props) => {
   const { isDark, toggleTheme } = useTheme()
   const priceData = useGetPriceData()
   const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
-  const profile = useGetLocalProfile()
+  // const profile = useGetLocalProfile()
 
   return (
     <UikitMenu
@@ -30,7 +30,7 @@ const Menu: React.FC = (props) => {
       langs={allLanguages}
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd}
-      profile={profile}
+      // profile={profile}
       {...props}
     />
   )
