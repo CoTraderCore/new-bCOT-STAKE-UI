ee0e62e9c52db3720cb12f4ee78b2b2e913ec800
RuslanMirov <ruslan777mirov@gmail.com>
2021-05-03 09:49:08 +0300

    Remove price

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { useWeb3React } from '@web3-react/core'
 import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
-import useGetPriceData from 'hooks/useGetPriceData'
+// import useGetPriceData from 'hooks/useGetPriceData'
 // import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import useAuth from 'hooks/useAuth'
 import links from './config'
@@ -14,8 +14,8 @@ const Menu: React.FC = (props) => {
   const { login, logout } = useAuth()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
-  const priceData = useGetPriceData()
-  const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
+  // const priceData = useGetPriceData()
+  // const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
   // const profile = useGetLocalProfile()
 
   return (
@@ -29,7 +29,7 @@ const Menu: React.FC = (props) => {
       currentLang={selectedLanguage?.code || ''}
       langs={allLanguages}
       setLang={setSelectedLanguage}
-      cakePriceUsd={cakePriceUsd}
+      // cakePriceUsd={cakePriceUsd}
       // profile={profile}
       {...props}
     />
